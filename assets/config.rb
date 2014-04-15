cd0f9b24f8b8386d565b65c24698508073321940
Eli Van Zoeren <eli@elivz.com>
2014-04-15 10:10:29 -0700

    Most of Gulp compiles now

--- assets/config.rb
+++
@@ -1,9 +0,0 @@
-require 'susy'
-
-# Set this to the root of your project when deployed:
-http_path = "/"
-css_dir = "css"
-sass_dir = "sass"
-images_dir = "images"
-javascripts_dir = "js"
-relative_assets = true
--- gulpfile.js
+++ gulpfile.js
@@ -1,39 +1,68 @@
-var gulp = require('gulp');
-var changed = require('gulp-changed');
-var jshint = require('gulp-jshint');
-var sass = require('gulp-sass');
-var autoprefixer = require('gulp-autoprefixer');
-var csso = require('gulp-csso');
-var concat = require('gulp-concat');
-var uglify = require('gulp-uglify');
-var rename = require('gulp-rename');
-var livereload = require('gulp-livereload');
-
-// Lint Task
-gulp.task('lint', function() {
-    return gulp.src('assets/js/src/*.js')
-        .pipe(jshint())
-        .pipe(jshint.reporter('default'));
-});
+var gulp = require('gulp'),
+    changed = require('gulp-changed'),
+    jshint = require('gulp-jshint'),
+    sass = require('gulp-ruby-sass'),
+    autoprefixer = require('gulp-autoprefixer'),
+    csso = require('gulp-csso'),
+    concat = require('gulp-concat'),
+    uglify = require('gulp-uglify'),
+    spritesmith = require("gulp.spritesmith"),
+    imagemin = require('gulp-imagemin'),
+    rename = require('gulp-rename'),
+    cache = require('gulp-cache'),
+    livereload = require('gulp-livereload');
 
 // Compile Our Sass
 gulp.task('styles', function() {
-    return gulp.src('assets/sass/*.scss')
-        .pipe(sass({ style:'expanded' }))
+    return gulp.src('assets/css/src/*.scss')
+        .pipe(sass({
+            sourcemap: true,
+            style: 'compact',
+            require: 'susy' ,
+            precision: 7
+        }))
         .pipe(autoprefixer())
+        .pipe(gulp.dest('assets/css'))
+        .pipe(rename({suffix: '.min'}))
         .pipe(csso())
         .pipe(gulp.dest('assets/css'));
 });
 
+// Lint Task
+gulp.task('lint', function() {
+    return gulp.src('assets/js/**/*.js')
+        .pipe(jshint())
+        .pipe(jshint.reporter('default'));
+});
+
 // Concatenate & Minify JS
 gulp.task('scripts', function() {
-    return gulp.src(['assets/js/*.js'])
-        .pipe(changed('assets/js'))
+    return gulp.src('assets/js/src/**/*.js')
         .pipe(concat('site.js'))
         .pipe(gulp.dest('assets/js'))
-        .pipe(rename('site.min.js'))
         .pipe(uglify())
+        .pipe(rename({suffix: '.min'}))
         .pipe(gulp.dest('assets/js'));
+});
+
+// Sprites
+gulp.task('sprites', function() {
+    var spriteData = gulp.src('assets/images/sprites/*.png').pipe(spritesmith({
+            imgName: 'sprites.png',
+            cssName: '_sprites.scss',
+            imgPath: '../images/sprites.png',
+            algorithm: 'binary-tree',
+            padding: 10
+        }));
+        spriteData.img.pipe(gulp.dest('assets/images/src/'));
+        spriteData.css.pipe(gulp.dest('assets/css/src/utilities/'));
+});
+
+// Images
+gulp.task('images', function() {
+    return gulp.src('assets/images/src/**/*')
+        .pipe(cache(imagemin({ optimizationLevel: 3, progressive: true, interlaced: true })))
+        .pipe(gulp.dest('assets/images'));
 });
 
 // Watch Files For Changes
@@ -42,10 +71,13 @@ gulp.task('watch', function() {
     gulp.watch('assets/js/src/**/*.js', ['lint', 'scripts']).on('change', function(file) {
         server.changed(file.path);
     });
-    gulp.watch('assets/sass/**/*.scss', ['styles']).on('change', function(file) {
+    gulp.watch('assets/css/src/**/*.scss', ['styles']).on('change', function(file) {
+        server.changed(file.path);
+    });
+    gulp.watch(['assets/images/src/**/*', 'assets/images/sprites/**/*'], ['images']).on('change', function(file) {
         server.changed(file.path);
     });
 });
 
 // Default Task
-gulp.task('default', ['lint', 'styles', 'scripts', 'watch']);
+gulp.task('default', ['lint', 'styles', 'scripts', 'sprites', 'images']);
