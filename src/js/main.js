cb243ac764cace33f8ff03f36ba74b2d466b2080
Eli Van Zoeren <eli@elivz.com>
2017-03-04 22:56:23 -0500

    More anlytics tracking

--- src/js/main.js
+++ src/js/main.js
@@ -1,6 +1,8 @@
 import $ from 'jquery';
 import 'a11y-toggle';
 
+// Reusable modules
+import analytics from 'plugins/analytics';
 import 'plugins/externalLinks';
 
 // Export jQuery to global namespace
@@ -8,3 +10,6 @@ window.$ = $;
 
 // Add "js" class to the html element
 document.documentElement.className = document.documentElement.className.replace('no-js', 'js');
+
+// Start tracking
+analytics.init();
