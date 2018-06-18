80bb36a40f7c0bf5b9a22e17ba2a114b65ec49bd
Eli Van Zoeren <eli@elivz.com>
2018-06-18 09:59:07 -0400

    Refactor Mix sync component

--- config/build/sync-component.js
+++ config/build/sync-component.js
@@ -1,39 +1,40 @@
-let mix = require('laravel-mix');
-let CopyWebpackPlugin = require('copy-webpack-plugin');
+const mix = require('laravel-mix');
 
 class Sync {
-    /**
-     * All dependencies that should be installed by Mix.
-     *
-     * @return {Array}
-     */
-    dependencies() {
-        return ['copy-webpack-plugin'];
-    }
+  /**
+   * All dependencies that should be installed by Mix.
+   *
+   * @return {Array}
+   */
+  dependencies() {
+    return ['copy-webpack-plugin'];
+  }
 
-    /**
-     * Register the component.
-     * @param  {*} ...params
-     * @return {void}
-     *
-     */
-    register(from, to) {
-        this.config = { from, to };
-    }
+  /**
+   * Register the component.
+   * @param  {string} from
+   * @param  {string} to
+   * @return {void}
+   *
+   */
+  register(from, to) {
+    this.paths = this.paths || [];
+    this.paths.push({ from, to });
+  }
 
-    /*
-     * Plugins to be merged with the master webpack config.
-     *
-     * @return {Array|Object}
-     */
-    webpackPlugins() {
-        return new CopyWebpackPlugin([
-            {
-                from: Mix.paths.root(this.config.from),
-                to: Mix.paths.root(this.config.to),
-            },
-        ]);
-    }
+  /*
+  * Plugins to be merged with the master webpack config.
+  *
+  * @return {Array|Object}
+  */
+  webpackPlugins() {
+    const CopyWebpackPlugin = require('copy-webpack-plugin');
+    const patterns = this.paths.map(path => ({
+      from: Mix.paths.root(path.from),
+      to: Mix.paths.root(path.to),
+    }));
+    return new CopyWebpackPlugin(patterns);
+  }
 }
 
 mix.extend('sync', new Sync());
